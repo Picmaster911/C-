86ede6e568e945d971a7ddb0ad392320505169be
Dmytro Sharshakov <113136582+Picmaster911@users.noreply.github.com>
2025-11-30 14:34:52 +0200

    Add HW3 SharedPoiter

---
+++ HW3/HW3.cpp
@@ -0,0 +1,40 @@
+﻿// HW3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "SharedPtrInt.h"
+
+void TestPtr(SharedPtrInt<int> val)
+{
+    std::cout << "CountPointer = " << val.GetCount() << "\n";
+}
+
+int main()
+{
+
+    SharedPtrInt<int> objB(new int(5));
+    SharedPtrInt<int> objA(new int(10));
+    std::cout << "ValueObjA =  " << objA.GetPtr() << "\n";
+    std::cout << "ValueObjB =  " << objB.GetPtr() << "\n";
+    std::cout << "CountPointer ObjB =  " << objB.GetCount() << "\n";
+    TestPtr(objB);
+    std::cout << "CountPointer ObjB =  "<< objB.GetCount() << "\n";
+    std::cout << "ValueObjB =  " << objB.GetPtr() << "\n";
+    std::cout << "ObjAisValid =  " << objA.isValid() << "  - CountPointer ObjA =  " << objA.GetCount() << "\n";
+    objB = objA;
+    std::cout << "ValueObjB =  " << objB.GetPtr() << "  - CountPointer ObjB =  " << objB.GetCount() << "\n";
+    std::cout << " (ObjB delete) -> ObjB references to ObjA (SharedPointer) " << "ObjAisValid =  " << objA.isValid() << "  - CountPointer ObjA =  " << objA.GetCount() << "\n";
+}
+
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ HW3/SharedPtrInt.h
@@ -0,0 +1,89 @@
+#pragma once
+template<typename A>
+class SharedPtrInt
+{
+private:
+	A* ptr;
+	size_t* count;
+public:
+	SharedPtrInt() : ptr(nullptr), count(nullptr) {}
+	SharedPtrInt(A* p) : ptr(p)
+	{
+		if (p)
+		{
+			count = new size_t(1);
+		}
+		else
+		{
+			count = nullptr;
+		}
+	}
+	SharedPtrInt(const A& value)
+		:ptr(new A(value))
+	{
+		count = new size_t(1);
+	}
+	~SharedPtrInt()
+	{
+		release();
+	}
+
+	void release()
+	{
+		if (count)
+		{
+			--(*count);
+			if (*count == 0)
+			{
+				delete ptr;
+				delete count;
+			}
+		}
+		ptr = nullptr;
+		count = nullptr;
+	}
+
+	// 1. void someFunc (SharedPtrInt<int> sp) //2.SharedPtrInt<int> a(new int(5)); SharedPtrInt<int> b(a);
+	SharedPtrInt(const SharedPtrInt& other)
+	{
+		ptr = other.ptr;
+		count = other.count;
+		if (count) ++(*count);
+	}
+	// Just a = b (1. delete a // 2. next b copy to a)
+	SharedPtrInt& operator=(const SharedPtrInt& other)
+	{
+		if (this == &other) return *this;
+		release();
+		ptr = other.ptr;
+		count = other.count;
+		if (count) ++(*count);
+		return *this;
+	}
+
+	SharedPtrInt(const SharedPtrInt&&) = delete;
+	SharedPtrInt& operator=(const SharedPtrInt&&) = delete;
+
+	bool isValid() const {
+		return (ptr != nullptr);
+	}
+	operator bool() const {
+		return (ptr != nullptr);
+	}
+
+	int GetCount()
+	{
+		if(count)
+		return *count;
+		else
+		{
+			return 0;
+		}
+	}
+
+	A GetPtr()
+	{
+		return *ptr;
+	}
+};
+
